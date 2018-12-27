4320c0bb5a2ef41fb4f8acda54aae952a0cddd11
sdoddam1 <sdoddam1@binghamton.edu>
2018-12-27 15:59:29 -0500

    Modifying mount_images.sh script

--- scripts/mount_images.sh
+++ scripts/mount_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mount | grep /home/sdoddam1/workspace/images &> /dev/null
+mount | grep ~/workspace/images &> /dev/null
 
 if [[ $? = 1 ]];
 then
-	mount -t nfs 10.128.0.2:/home/spoorti/vfresh-new-images/ /home/sdoddam1/workspace/images/
+	mount -t nfs 10.128.0.2:/home/spoorti/vfresh-new-images/ ~/workspace/images/
 fi
