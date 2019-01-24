dc2cb763085a0eeb454809cf8388f4368a2c4d3c
sdoddam1 <sdoddam1@binghamton.edu>
2019-01-24 09:19:34 -0500

    Modifying insert module script to include kvm modules

--- scripts/insert_modules.sh
+++ scripts/insert_modules.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-sudo rmmod kvm
 sudo rmmod kvm_intel
-
+sudo rmmod kvm
 
 lsmod | grep -ie "kvm" &> /dev/null
 if [[ $? = 1 ]];
 then
 	modprobe kvm
+fi
+
+lsmod | grep -ie "kvm_intel" &> /dev/null
+if [[ $? = 1 ]];
+then
 	modprobe kvm_intel nested=0
 fi
 
 lsmod | grep -ie "vhost-net" &> /dev/null
-
 if [[ $? = 1 ]];
 then
 	modprobe vhost-net
 fi
 
 lsmod | grep -ie "vfio-pci" &> /dev/null
-
 if [[ $? = 1 ]];
 then
 	modprobe vfio-pci
 fi
 
 lsmod | grep -ie "igbvf" &> /dev/null
-
 if [[ $? = 1 ]]
 then
 	modprobe igbvf
